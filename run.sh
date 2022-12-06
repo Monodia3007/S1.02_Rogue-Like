1b75f9225ac308973090a98e9da6ca212bceb990
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-06 14:41:24 +0100

    Modified run.sh and compile.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd classes
-export CLASSPATH=`find ../lib -name "*.jar" | tr '\n' ':'`
-java -cp ${CLASSPATH}:. Main
+export CLASSPATH=$(find ../lib -name "*.jar" | tr '\n' ':')
+java -cp "${CLASSPATH}":. Main
 cd ..
