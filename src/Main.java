5cef6a07d1f73b68b908872796831e59e8e975b0
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-06 14:58:27 +0100

    reset src on main branch

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,3 @@
 class Main extends Program {
-    void initialiserDonjon () {
 
-    }
-
-    void algorithm () {
-        int largeur, longueur;
-        println("Quelle taille de donjon voulez vous ? (1 n'est pas une taille valide)");
-        while (largeur <= 1 && longueur <= 1) {
-            print("Longueur : ")
-            longueur = readInt();
-            print("Largeur : ")
-            largeur = readInt();
-        }
-        int[][] donjon = new
-    }
 }
