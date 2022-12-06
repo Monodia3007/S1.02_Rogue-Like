964fe846e917412c37acd043ac69a4ec6c1b6db1
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-06 14:24:14 +0100

    Edits on the files

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ class Main extends Program {
     }
 
     void algorithm () {
-        int largeur, longueur;
+        int largeur = 0, longueur = 0;
         println("Quelle taille de donjon voulez vous ? (1 n'est pas une taille valide)");
         while (largeur <= 1 && longueur <= 1) {
-            print("Longueur : ")
+            print("Longueur : ");
             longueur = readInt();
-            print("Largeur : ")
+            print("Largeur : ");
             largeur = readInt();
         }
-        int[][] donjon = new
+        int[][] donjon = new int[longueur][largeur];
     }
 }
