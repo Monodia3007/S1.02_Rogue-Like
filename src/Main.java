fcae50505d304b6fc0ea52e07727fe36fe0280a7
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-06 15:04:57 +0100

    Added RGBToANSI to Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ class Main extends Program {
 
     }
 
+    String RGBToANSI(int[] rgb, boolean backgroundColor) {
+        return "\u001b[" + (backgroundColor ? "48" : "38") + ";2;" + rgb[0] + ";" + rgb[1] + ";" + rgb[2] + "m";
+    }
+
     void algorithm () {
         int largeur = 0, longueur = 0;
-        println("Quelle taille de donjon voulez vous ? (1 n'est pas une taille valide)");
+        println("Quelle taille de donjon voulez vous ?");
         while (largeur <= 1 && longueur <= 1) {
             print("Longueur : ");
             longueur = readInt();
