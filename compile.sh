8a400943aceb86a0b260fe0febdde8adb494f353
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-06 14:55:55 +0100

    Modified run.sh and compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd classes || exit
-export CLASSPATH=$(find ../lib -name "*.jar" | tr '\n' ':')
-java -cp "${CLASSPATH}":. Main
-cd ..
+export SOURCES=src
+export CLASSES=classes
+export CLASSPATH=$(find lib -name "*.jar" | tr '\n' ':')
+
+javac -cp "${CLASSPATH}" -sourcepath ${SOURCES} -d ${CLASSES} "$@" "find src -name "*.java""
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd classes
-export CLASSPATH=`find ../lib -name "*.jar" | tr '\n' ':'`
-java -cp ${CLASSPATH}:. Main
+cd classes || exit
+export CLASSPATH=$(find ../lib -name "*.jar" | tr '\n' ':')
+java -cp "${CLASSPATH}":. Main
 cd ..
