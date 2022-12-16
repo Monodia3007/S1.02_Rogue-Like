c68b009020ce0ac8015c17e0bae58b107dc1e465
Pierre Camplin <pierre.camplin@gmail.com>
2022-12-16 11:57:07 +0100

    $

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,35 @@
 class Main extends Program {
 
     final int COTE = 21;
+    final Piece[] PIECES = new Piece[]{
+            newPiece('V', 0.0),
+            newPiece('S', 0.0),
+            newPiece('R', 0.6),
+            newPiece('H', 0.0),
+            newPiece('B', 0.0)
+    }
+    final int NB_PIECE_PAR_ETAGE = 10;
 
     int random(int min, int max) {
         int range = max - min;
         return (int) (random() * range) + min;
     }
-    void initialiserDonjon (char[][] donjon) {
+
+    Donjon newDonjon() {
+        Donjon donjon = new Donjon();
+        donjon.etageActuel = new Piece[COTE][COTE];
+        donjon.numeroEtage = 0;
+        return donjon;
+    }
+
+    Piece newPiece (char type, double spawnRate) {
+        Piece piece = new Piece();
+        piece.type = type;
+        piece.spawnRate = spawnRate;
+        return piece;
+    }
+
+    void initialiserDonjon (Donjon donjon) {
 
     }
 
@@ -15,6 +38,6 @@ class Main extends Program {
     }
 
     void algorithm () {
-        char[][] donjon = new char[COTE][COTE];
+        Donjon donjon = newDonjon();
     }
 }
