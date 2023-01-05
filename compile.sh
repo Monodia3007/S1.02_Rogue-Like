adafb0ee3d4a962bb0f45ef4f9ecd295d0d8a378
Pierre Camplin <pierre.camplin@gmail.com>
2023-01-05 09:41:54 +0100

    Modification des scripts de lancement et de compilation

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@ export CLASSES=classes
 export CLASSPATH=`find lib -name "*.jar" | tr '\n' ':'`
 
 javac -cp ${CLASSPATH} -sourcepath ${SOURCES} -d ${CLASSES} $@ `find src -name "*.java"`
+cp ressources/* classes/
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd classes || exit
-export CLASSPATH=$(find ../lib -name "*.jar" | tr '\n' ':')
-java -cp "${CLASSPATH}":. Main
+cp ressources/* classes/
+cd classes
+export CLASSPATH=`find ../lib -name "*.jar" | tr '\n' ':'`
+java -cp ${CLASSPATH}:. $@
 cd ..
